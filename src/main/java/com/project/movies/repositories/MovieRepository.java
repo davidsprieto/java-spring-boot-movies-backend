ef35416c58b3b539968f78ca76b8be7741981d2a
David Prieto <david.s.prieto1@gmail.com>
2023-07-28 16:02:23 -0500

    created project structure and started creating endpoints

---
+++ src/main/java/com/project/movies/repositories/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.project.movies.repositories;
+
+import com.project.movies.models.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
